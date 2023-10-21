1b45b86caf408458ade7511b46cb7b7db58183be
joshua salceda <joshuasalceda0021@gmail.com>
2023-10-21 21:46:49 +0800

    Started developing the frontend

--- sync/views.py
+++ sync/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'pages/home.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
