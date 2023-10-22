51baf819e3bd047a172bdbab92a72fea8df37949
joshua salceda <joshuasalceda0021@gmail.com>
2023-10-22 15:41:21 +0800

    added responsiveness

--- sync/urls.py
+++ sync/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name="about"),
+    path('login/', views.login, name="login"),
+    path('register/', views.register, name="register")
 ]
--- sync/views.py
+++ sync/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
 def home(request):
     return render(request, 'pages/home.html')
 
 def about(request):
     return render(request, 'pages/about.html')
+
+def login(request):
+    return render(request, 'pages/login.html')
+
+def register(request):
+    return render(request, 'pages/register.html')   
+
