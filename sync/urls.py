47c9db07ff83e0f0fcbd32d9f1ec8679800cc0f6
Joshua <joshuasalceda0021@gmail.com>
2024-01-31 18:00:54 +0800

    some ui changes

--- sync/urls.py
+++ sync/urls.py
@@ -15,6 +15,8 @@ urlpatterns = [
     # Post
     #=========================================================
     path('post/new/', views.createPost, name='create_post'),
+    path('post/<int:post_id>/edit/', views.editPost, name='edit_post'),
+    path('delete/<int:post_id>/', views.deletePost, name='delete_post'),
     # path('post/<int:pk>/', views.postDetail, name='post_detail'),
 
 ]
--- sync/views.py
+++ sync/views.py
@@ -2,25 +2,53 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PostForm
 from .models import Post, PostImage
 
+#=========================================================
+# Home Page
+#=========================================================
+
 def home(request):
     return render(request, 'pages/home.html')
+
+#=========================================================
+# About Page
+#=========================================================
 
 def about(request):
     return render(request, 'pages/about.html')
 
+#=========================================================
+# Login Page
+#=========================================================
+
 def login(request):
     return render(request, 'pages/login.html')
 
+#=========================================================
+# Register Page
+#=========================================================
+
 def register(request):
     return render(request, 'pages/register.html')   
+
+#=========================================================
+# Feed Page
+#=========================================================
 
 def feed(request):
     posts = Post.objects.all()
     return render(request, 'pages/feed.html', {'posts': posts})
     # return render(request, 'pages/feed.html')
 
+#=========================================================
+# Profile Page
+#=========================================================
+
 def profile(request):
     return render(request, 'pages/profile.html')
+
+#=========================================================
+# Create Post
+#=========================================================
 
 def createPost(request):
     if request.method == 'POST':
@@ -28,18 +56,17 @@ def createPost(request):
         print(request.POST)
         print(request.FILES)
         if form.is_valid():
-            post = form.save()
-            
+            post = form.save()    
             handle_image_uploads(request, post)
-            # Handle multiple file uploads using request.FILES.getlist
-            # for image_file in request.FILES.getlist('images'):
-            #     PostImage.objects.create(post=post, file=image_file)
-
-            return redirect('create_post')  # Redirect to a view that displays the list of posts
+            return redirect('create_post')
     else:
         form = PostForm()
 
     return render(request, 'pages/post/create.html', {'form': form})
+
+#=========================================================
+# Edit Post
+#=========================================================
 
 def editPost(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
@@ -49,12 +76,25 @@ def editPost(request, post_id):
         if form.is_valid():
             post = form.save()
             handle_image_uploads(request, post)
-            return redirect('post_list')  # Redirect to a view that displays the list of posts
+            return redirect('edit_post', post_id=post.id)  
     else:
         form = PostForm(instance=post)
 
-    return render(request, 'edit_post.html', {'form': form, 'post': post})
+    return render(request, 'pages/post/edit.html', {'form': form, 'post': post})
+
+#=========================================================
+# Handle Multple Image Upload
+#=========================================================
 
 def handle_image_uploads(request, post):
     for image_file in request.FILES.getlist('images'):
         PostImage.objects.create(post=post, file=image_file)
+
+#=========================================================
+# Delet Post
+#=========================================================
+
+def deletePost(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    post.delete()
+    return redirect('feed')
